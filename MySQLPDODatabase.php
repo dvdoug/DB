6ec4305d069175b9325223c40d47b03dbf7cfa1b
Doug Wright <github@dougweb.org>
2013-01-06 17:14:12 +0000

    MySQL-specific PDO bits

---
+++ MySQLPDODatabase.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Database Access Layer
+ * @package DB
+ * @author Doug Wright
+ */
+  namespace DVDoug\DB;
+
+  /**
+   * MySQL database connection
+   * @author Doug Wright
+   * @package DB
+   */
+  class MySQLPDODatabase extends PDODatabase {
+    
+    public function __construct($aHost, $aPort, $aDefaultDatabase, $aUsername, $aPassword) {
+      parent::__construct("mysql:host={$aHost};port={$aPort};dbname={$aDefaultDatabase};charset=utf8", $aUsername, $aPassword);
+    }
+  }
